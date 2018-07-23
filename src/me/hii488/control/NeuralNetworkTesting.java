0c8592c11993b02faf5db79c09f670433fb727f6
Laurence <hii488@btinternet.com>
2018-07-23 16:53:56 +0100

    Added another test
    Also tried to do a binary encoding test which failed, not sure why :/

--- src/me/hii488/control/NeuralNetworkTesting.java
+++ src/me/hii488/control/NeuralNetworkTesting.java
@@ -5,7 +5,7 @@ import me.hii488.tests.StandardNetworkTest;
 public class NeuralNetworkTesting {
 	
 	public static void main(String[] args) {
-		System.out.println(StandardNetworkTest.test());
+		System.out.println(StandardNetworkTest.simpleTest2());
 	}
 	
 }
--- src/me/hii488/tests/StandardNetworkTest.java
+++ src/me/hii488/tests/StandardNetworkTest.java
@@ -1,5 +1,9 @@
 package me.hii488.tests;
 
+import java.util.Arrays;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 import me.hii488.control.data.DataController;
 import me.hii488.control.trainers.StandardTrainer;
 import me.hii488.control.trainers.Trainer;
@@ -9,7 +13,7 @@ import me.hii488.network.nodeActivations.SigmoidActivation;
 
 public class StandardNetworkTest {
 	
-	public static boolean test() {
+	public static boolean simpleTest() {
 		Network net = new Network(10, new int[] {16, 8}, 1, new SigmoidActivation());
 		net.randomise();
 		
@@ -49,21 +53,68 @@ public class StandardNetworkTest {
 		};
 		
 		Trainer t = new StandardTrainer(net, data, new QuadraticCost(), 1);
+		return doTheTests(t);
+	}
+	
+	public static boolean simpleTest2() {
+		Network net = new Network(10, new int[] {16, 8}, 10, new SigmoidActivation());
+		net.randomise();
 		
+		DataController data = new DataController() {
+			int inputs = 10;
+			double expectedOut;
+			
+			@Override
+			public double[] getNextTrainingInput() {
+				double[] d = new double[inputs];
+				for(int i = 0; i < inputs; i++) {
+					d[i] = Network.random.nextInt(1);
+					expectedOut += d[i];
+				}
+				
+				return d;
+			}
+
+			@Override
+			public double[] getNextTestingInput() {
+				return getNextTrainingInput();
+			}
+
+			@Override
+			public double[] getCurrentExpectedOutput() {
+				double[] d = new double[10]; // there are 10 outputs
+				for(int i = 0; i < d.length; i++) {
+					if(i==expectedOut) d[i] = 1;
+					else d[i] = 0;
+				}
+				return d;
+			}
+
+			@Override
+			public void setInputSize(int i) {
+				inputs = i;
+			}
+			
+		};
 		
-		System.out.println(t.test(1000));
-		t.train(1, 30);
-		System.out.println(t.test(1000));
-		t.train(5, 30);
-		System.out.println(t.test(300));
-		t.train(20, 30);
-		System.out.println(t.test(300));
+		Trainer t = new StandardTrainer(net, data, new QuadraticCost(), 1);
+		return doTheTests(t);
+	}
+	
+	private static boolean doTheTests(Trainer t) {
+		System.out.println(t.test(100));
 		t.train(100, 30);
-		System.out.println(t.test(300));
+		System.out.println(t.test(1000));
+		t.train(100, 30);
+		System.out.println(t.test(1000));
+		t.train(100, 30);
+		System.out.println(t.test(1000));
+		t.train(100, 30);
+		System.out.println(t.test(1000));
 		t.train(1000, 30);
-		System.out.println(t.test(300));
+		System.out.println(t.test(1000));
 		t.train(1000, 30);
-		System.out.println(t.test(300));
+		System.out.println(t.test(1000));
 		
 		return t.test(60) > 0.9;
 	}
